dc973c4d61b6bf4619a2845ef0ef9f655e666494
MrKahr <madskhebsgaard@gmail.com>
2025-10-13 01:58:01 +0200

    bugfixing but still no terminal output

--- projects/odin_rps/game_logic_minimal.js
+++ projects/odin_rps/game_logic_minimal.js
@@ -1,5 +1,4 @@
-// Challenge, do in a few lines as possible 
-const choices = ["ROCK, PAPER, SCISSORS"];
+const choices = ["ROCK","PAPER", "SCISSORS"];
 
 const wins = {
     ROCK: "SCISSORS",
@@ -7,20 +6,20 @@ const wins = {
     SCISSORS: "PAPER"
 };
 
-const getComputerChoice = () =>  choices[Math.floor(Math.random() * choices.length)];
+const getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
 
 const getHumanChoice = () => prompt("Choose rock, paper or scissors").toUpperCase();
 
 const playRound = (humanChoice, computerChoice) => {
-    console.log(`Human chose ${humanChoice}! Computer chose ${computerChoice}`);
-    if(humanChoice === computerChoice){(console.log("It's a tie!"),0)}
-    else{wins[humanChoice] === computerChoice ? (console("Human wins!") , 1): ((console.log("Computer wins!"), -1));}
+    console.log(`Human chose ${humanChoice}! Computer chose ${computerChoice}`)
+    if(humanChoice === computerChoice){return 0;}
+    else{return wins[humanChoice] === computerChoice ? 1 : -1;}
 };
 
 const playGame = (totalRounds) => {
     let scoreDifference = 0;
-    for (let i = 0; i  < totalRounds; i++){playRound(getComputerChoice(), getHumanChoice())};
-    scoreDifference === 0 ? console.log("It's a tie!") : (scoreDifference < 0 ? `Computer wins by ${scoreDifference}! ` : `Human wins by ${scoreDifference}!`) 
+    for (let i = 0; i  < totalRounds; i++){scoreDifference += playRound(getHumanChoice(), getComputerChoice())};
+    scoreDifference === 0 ? console.log("Nobody wins!") : (scoreDifference < 0 ? console.log(`Computer wins by ${Math.abs(scoreDifference)}`) : console.log(`Human wins by ${Math.abs(scoreDifference)}!`));
 }
 
 playGame(2); 
