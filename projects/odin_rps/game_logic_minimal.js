3d688bd8e6a971c3968d190a1698ee5880cb047a
MrKahr <madskhebsgaard@gmail.com>
2025-10-23 15:29:09 +0200

    Discovered and fixed single syntax error

--- projects/odin_rps/game_logic_minimal.js
+++ projects/odin_rps/game_logic_minimal.js
@@ -1,4 +1,4 @@
-const choices = ["ROCK","PAPER", "SCISSORS"];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 const wins = {
     ROCK: "SCISSORS",
@@ -12,14 +12,14 @@ const getHumanChoice = () => prompt("Choose rock, paper or scissors").toUpperCas
 
 const playRound = (humanChoice, computerChoice) => {
     console.log(`Human chose ${humanChoice}! Computer chose ${computerChoice}`)
-    if(humanChoice === computerChoice){return 0;}
-    else{return wins[humanChoice] === computerChoice ? 1 : -1;}
+    if (humanChoice === computerChoice) { return 0; }
+    else { return wins[humanChoice] === computerChoice ? 1 : -1; }
 };
 
 const playGame = (totalRounds) => {
     let scoreDifference = 0;
-    for (let i = 0; i  < totalRounds; i++){scoreDifference += playRound(getHumanChoice(), getComputerChoice())};
-    scorereDifference === 0 ? (console.log("Nobody wins!")) : (scoreDiffsrence < 0 ? (console.log(`Computer wins by ${Math.abs(scoreDifference)}`)) : (console.log(`Human wins by ${Math.abs(scoreDifference)}!`)));
+    for (let i = 0; i < totalRounds; i++) { scoreDifference += playRound(getHumanChoice(), getComputerChoice()) };
+    scoreDifference === 0 ? (console.log("Nobody wins!")) : (scoreDifference < 0 ? (console.log(`Computer wins by ${Math.abs(scoreDifference)}`)) : (console.log(`Human wins by ${Math.abs(scoreDifference)}!`)));
 }
 
 playGame(2); 
